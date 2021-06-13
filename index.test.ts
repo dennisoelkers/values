988d7b1a2422d5c1e4e85d133aa317a5912a6341
Dennis Oelkers <dennis@graylog.com>
2021-06-13 10:30:14 +0200

    Improving typing, adding tests.

---
+++ index.test.ts
@@ -0,0 +1,61 @@
+import { createValue } from "./index";
+
+type MeasurementOptions = {
+  magnitude: number,
+  unit: 'cm' | 'ft',
+};
+
+const defaults = { magnitude: undefined, unit: 'cm' as 'cm' };
+const Measurement = createValue<MeasurementOptions, typeof defaults>(defaults);
+
+describe("Measurement", () => {
+  it("allows creating instance", () => {
+    const measurement = Measurement({ magnitude: 100, unit: "cm" });
+    expect(measurement.magnitude).toEqual(100);
+    expect(measurement.unit).toEqual("cm");
+  });
+
+  it("two instances with same values are equal", () => {
+    const measurement = Measurement({ magnitude: 100, unit: "cm" });
+    const measurement2 = Measurement({ magnitude: 100, unit: "cm" });
+
+    expect(measurement).toEqual(measurement2);
+  });
+
+  it("two instances with different values are not equal", () => {
+    const measurement = Measurement({ magnitude: 100, unit: "cm" });
+    const measurement2 = Measurement({ magnitude: 200, unit: "ft" });
+
+    expect(measurement).not.toEqual(measurement2);
+  });
+
+  it("creating instance with builder is the same as creating from scratch", () => {
+    const measurement = Measurement({ magnitude: 100, unit: "cm" });
+    const measurement2 = Measurement({ magnitude: 200, unit: "ft" })
+      .toBuilder()
+      .magnitude(100)
+      .unit("cm")
+      .build();
+
+    expect(measurement).toEqual(measurement2);
+  });
+
+  it("does not permit setting attributes directly", () => {
+    const measurement = Measurement({ magnitude: 100, unit: "cm" });
+    expect(() => {
+      measurement.magnitude = 200;
+    }).toThrowErrorMatchingInlineSnapshot(
+      `"Cannot assign to read only property 'magnitude' of object '#<Object>'"`
+    );
+  });
+
+  it("uses defaults if property is not specified", () => {
+    const measurement = Measurement({ magnitude: 100 });
+    expect(measurement).toEqual(Measurement({ magnitude: 100, unit: 'cm' }));
+  });
+ 
+  it("throws type error if required property is not specified", () => {
+    // @ts-expect-error should complain about missing magnitude
+    const measurement = Measurement({ unit: 'cm' });
+  });
+ });
---
+++ index.ts
@@ -0,0 +1,55 @@
+type Builder<T> = Readonly<{ [key in keyof T]: (value: T[key]) => Builder<T> } & { build: () => ValueObject<T> }>;
+
+type ValueObject<T> = T & { toBuilder: () => Builder<T> };
+
+const builder = <T>(defaults: Defaults<T>, obj: T, factory: (v: T) => ValueObject<T>): Builder<T> => {
+  // @ts-ignore
+  const _builder: {
+    [key in keyof T]: (value: T[key]) => Builder<T>
+  } = Object.keys(defaults).map(prop => ({
+    get [prop]() {
+      return (value: any) => builder<T>(defaults, { ...obj, [prop]: value }, factory);
+    }
+  }))
+    .reduce((prev, cur) => ({ ...prev, ...cur }), {});
+  
+  return Object.freeze({
+    ..._builder,
+    build: () => factory(obj),
+  });
+};
+
+
+type Defaults<T> = { [key in keyof T]: T[key] | undefined };
+
+type UndefinedKeys<T> = { [P in keyof T]: T[P] extends typeof undefined ? P : never }[keyof T];
+type DefinedKeys<T> = { [P in keyof T]: T[P] extends typeof undefined ? never : P }[keyof T];
+
+type RequiredKeys<T, U extends keyof T> = Pick<T, U>;
+type OptionalKeys<T, U extends keyof T> = { [P in keyof Pick<T, U>]?: T[P] }
+
+type MeasurementOptions = {
+  magnitude: number,
+  unit: 'cm' | 'ft',
+};
+
+const MD = { magnitude: undefined, unit: 'cm' };
+type MeasurementDefaults = typeof MD;
+
+type Foo = DefinedKeys<MeasurementDefaults>;
+type Bar = UndefinedKeys<MeasurementDefaults>;
+type Defaultized = RequiredKeys<MeasurementOptions, UndefinedKeys<MeasurementDefaults>> & OptionalKeys<MeasurementOptions, DefinedKeys<MeasurementDefaults>>;
+
+export const createValue = <T extends {}, D extends Defaults<T>>(defaults: D) => {
+  function _Value(props: RequiredKeys<T, UndefinedKeys<D>> & OptionalKeys<T, DefinedKeys<D>>): ValueObject<T> {
+    const value = Object.assign({}, defaults, props);
+    const Value = {
+      toBuilder: () => builder(defaults, value, _Value),
+    };
+    Object.setPrototypeOf(value, Value);
+    return Object.freeze(value);
+  }
+  
+  return _Value;
+};
+
